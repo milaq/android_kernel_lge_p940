387c78bde6eab706c37c41d8e4e91b9265e8df1c
Roman Shaposhnikov <x0166637@ti.com>
2012-06-13 23:00:26 +0300

    Merge latest changes from google kernel/common.git
    Kernel version updated to 3.0.31
    
    Change-Id: Ifbd7150801f3beeec9cbaa566f249d8019ef9348
    Signed-off-by: Roman Shaposhnikov <x0166637@ti.com>
    
    Conflicts:
    	drivers/usb/gadget/android.c
    	fs/proc/base.c
    	include/trace/events/cpufreq_interactive.h

--- fs/proc/base.c
+++ fs/proc/base.c
@@ -211,12 +211,8 @@ static struct mm_struct *mm_access(struct task_struct *task, unsigned int mode)
 
 	mm = get_task_mm(task);
 	if (mm && mm != current->mm &&
-#if defined(CONFIG_MACH_LGE_P2)
 			!ptrace_may_access(task, mode) &&
 			!capable(CAP_SYS_RESOURCE)) {
-#else
-			!ptrace_may_access(task, mode)) {
-#endif
 		mmput(mm);
 		mm = ERR_PTR(-EACCES);
 	}
@@ -858,11 +854,16 @@ static ssize_t mem_read(struct file *file, char __user *buf,
 	return mem_rw(file, buf, count, ppos, 0);
 }
 
+#define mem_write NULL
+
+#ifndef mem_write
+/* This is a security hazard */
 static ssize_t mem_write(struct file *file, const char __user *buf,
 			 size_t count, loff_t *ppos)
 {
 	return mem_rw(file, (char __user*)buf, count, ppos, 1);
 }
+#endif
 
 loff_t mem_lseek(struct file *file, loff_t offset, int orig)
 {
