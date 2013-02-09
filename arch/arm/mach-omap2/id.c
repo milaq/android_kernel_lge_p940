433ccca834954a8ddfc0c91ebc99fa654f4ee613
milaq <micha.laqua@gmail.com>
2013-02-09 17:05:34 +0100

    OMAP4: intruduce silicon type override when set, this enables basic overclocking capabilites. use with care!
    most (if not all) of the omap4xxx SOCs should be able to reach higher frequencies. with smartreflex and tempgov in place this should also be safe on the respective platforms.
    
    in omap4_check_features(void) OMAP4_SILICON_TYPE_PERFORMANCE is enforced, which enables higher MPU (on 446x also IVA) frequencies regardless of the actual silicon type read from die and production IDs

--- arch/arm/mach-omap2/id.c
+++ arch/arm/mach-omap2/id.c
@@ -233,8 +233,13 @@ static void __init omap4_check_features(void)
 
 	omap4_features = 0;
 
+#if defined(CONFIG_OMAP4_OVERCLOCK)
+	printk(KERN_ERR "OMAP4: enabled performance type silicon clockrates\n");
+	si_type = OMAP4_SILICON_TYPE_PERFORMANCE;
+#else
 	si_type =
 	  (read_tap_reg(OMAP4_CTRL_MODULE_CORE_STD_FUSE_PROD_ID_1) >> 16) & 3;
+#endif
 
 	switch (si_type) {
 	case OMAP4_SILICON_TYPE_PERFORMANCE:
