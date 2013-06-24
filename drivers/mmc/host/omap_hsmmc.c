990ac87e5dc68c898a42f999e88560963e807f4f
milaq <micha.laqua@gmail.com>
2013-06-24 15:54:19 +0200

    Revert "omap-hsmmc: Prevent possible race condition"
    This reverts commit 1db70565494809bf67e99969f95fbf0750ceab1a.

--- drivers/mmc/host/omap_hsmmc.c
+++ drivers/mmc/host/omap_hsmmc.c
@@ -2578,7 +2578,7 @@ static int omap_hsmmc_remove(struct platform_device *pdev)
 	struct resource *res;
 
 	if (host) {
-		mmc_claim_host(host->mmc);
+		mmc_host_enable(host->mmc);
 		mmc_remove_host(host->mmc);
 		if (host->use_reg)
 			omap_hsmmc_reg_put(host);
@@ -2593,7 +2593,7 @@ static int omap_hsmmc_remove(struct platform_device *pdev)
 			dma_free_coherent(NULL, ADMA_TABLE_SZ,
 				host->adma_table, host->phy_adma_table);
 
-		mmc_release_host(host->mmc);
+		mmc_host_disable(host->mmc);
 		pm_runtime_suspend(host->dev);
 
 		clk_put(host->fclk);
@@ -2647,11 +2647,11 @@ static int omap_hsmmc_suspend(struct device *dev)
 			host->mmc->pm_flags |= MMC_PM_KEEP_POWER;
 		ret = mmc_suspend_host(host->mmc);
 		if (ret == 0) {
-			mmc_claim_host(host->mmc);
+			mmc_host_enable(host->mmc);
 			omap_hsmmc_disable_irq(host);
 			OMAP_HSMMC_WRITE(host->base, HCTL,
 				OMAP_HSMMC_READ(host->base, HCTL) & ~SDBP);
-			mmc_release_host(host->mmc);
+			mmc_host_disable(host->mmc);
 
 			if (host->got_dbclk)
 				clk_disable(host->dbclk);
@@ -2684,7 +2684,9 @@ static int omap_hsmmc_resume(struct device *dev)
 		return 0;
 
 	if (host) {
-		mmc_claim_host(host->mmc);
+		if (mmc_host_enable(host->mmc) != 0) {
+			goto clk_en_err;
+		}
 
 		if (host->got_dbclk)
 			clk_enable(host->dbclk);
@@ -2705,9 +2707,14 @@ static int omap_hsmmc_resume(struct device *dev)
 		if (ret == 0)
 			host->suspended = 0;
 
-		mmc_release_host(host->mmc);
+		mmc_host_lazy_disable(host->mmc);
 	}
 
+	return ret;
+
+clk_en_err:
+	dev_dbg(mmc_dev(host->mmc),
+		"Failed to enable MMC clocks during resume\n");
 	return ret;
 }
 
